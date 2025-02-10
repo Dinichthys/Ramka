f89b768e8f0729e5b1dde501409370b9b6f5f243
dinichthys <den2005425@gmail.com>
2025-02-11 02:04:20 +0300

    Text

--- ramka.asm
+++ ramka.asm
@@ -4,20 +4,20 @@
 locals ww
 org 100h
 
-VIDEOSEG    equ 0b800h
+VIDEOSEG        equ 0b800h
+TOP_GAP         equ 3h
+LEFT_GAP        equ 3h
 
 ;----------MAIN-------------------
 Main:
         call SetVideoseg
 
-        mov ah, 0Ah                     ; 10 * 10  | AL - x | AH - y |
-        mov al, 0Ah                     ;
+        mov ah, 0Bh                     ; 10 * 10  | AL - x | AH - y |
+        mov al, 30h                     ;
         mov dx, offset String           ; Text in dx
         mov bx, offset Base             ; Base of the Frame in bx
 
         call DrawFrame
-
-        call PrintText
 
         mov ax, 4c00h
         int 21h         ; exit
@@ -26,7 +26,7 @@ Main:
 
 ;--------------DATA---------------
 
-String: db 'Enter the text$'
+String: db 'I am inside the box', 0Ah, 0Ah, 0Ah, '                 please, help me', 0h
 
 Base: db '@-@|=|@-@'
 
@@ -72,16 +72,16 @@ DrawFrame     proc
         mov cl, 50h             ; 50h = 80
         sub cl, al
         shr cl, 01h
-        sub cl, 01h
         add al, cl
         dec al
 
         mov ch, 19h             ; 19h = 25
         sub ch, ah
         shr ch, 01h
-        sub ch, 01h
         add ah, ch
         dec ah
+
+        push cx                 ; Save the starting coordinates
 
         push ax
         call BaseLine
@@ -108,7 +108,12 @@ wwEnd:
         call BaseLine
         pop ax
 
+        pop cx
         pop dx
+
+        push ax
+        call PrintText
+        pop ax
 
         ret
 
@@ -188,6 +193,67 @@ wwFor_end:
         inc bx
 
         ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; Print the text pointed in dx
+; to the frame with
+; starting coordinates
+; CL - x, CH - y
+;
+; Entry:  ES, DX, CH, CL
+; Exit:   None
+; Destrs: AX, BX
+;---------------------------------
+
+PrintText     proc
+
+        mov bl, ch
+        mov bh, 0h
+        add bx, TOP_GAP
+        imul bx, bx, 50h
+        shl bx, 01h
+        mov al, cl
+        mov ah, 0h
+        shl ax, 01h
+        add bx, ax                      ; bx - index in memory of starting the string
+        add bx, LEFT_GAP * 2
+
+        push si
+        mov si, dx
+
+        mov ah, 0h
+wwCond:
+        cmp byte ptr [si], 0h
+        je wwEnd
+        cmp byte ptr [si], 0Ah          ; \n
+        je wwNewLine
+
+wwWhile:
+        inc ah
+        mov al, byte ptr [si]
+        mov byte ptr es:[bx], al
+        add bx, 02h
+        inc si
+
+        jmp wwCond
+
+wwEnd:
+
+        pop si
+        ret
+
+wwNewLine:
+        inc si
+        add bx, 80 * 2
+        shl ah, 01h
+        sub bl, ah
+        mov ah, 0h
+        jmp wwCond
 
 endp
 
