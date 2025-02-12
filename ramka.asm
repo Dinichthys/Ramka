348cb709749b2b0043aa4004450844ccce5730ae
dinichthys <den2005425@gmail.com>
2025-02-13 00:37:49 +0300

    Argv

--- ramka.asm
+++ ramka.asm
@@ -8,17 +8,30 @@ VIDEOSEG        equ 0b800h
 TOP_GAP         equ 3h
 LEFT_GAP        equ 3h
 
+END_TEXT        equ 0Dh
+
 HORIZON_TER     equ 50h
 VERTICAL_TER    equ 19h
 
+ARGC_INDEX      equ 80h
+
+ARGV_INDEX      equ 82h
+
+SEP_CHAR        equ ' '         ; 32h = ' '
+END_SYM         equ 0Dh         ; 0Dh = '\r'
+
 ;----------MAIN-------------------
 Main:
-        call SetVideoseg
+        call GetArgs                    ; format: x y 'base' 'string'
 
-        mov ah, 0Bh                     ; 10 * 10  | AL - x | AH - y |
-        mov al, 30h                     ;
-        mov dx, offset String           ; Text in dx
-        mov bx, offset Base             ; Base of the Frame in bx
+        push bx
+        call SetVideoseg
+        pop bx
+
+        ; mov ah, 0Bh                     ; 10 * 10  | AL - x | AH - y |
+        ; mov al, 30h                     ;
+        ; mov dx, offset String           ; Text in dx
+        ; mov bx, offset Base             ; Base of the Frame in bx
 
         call DrawFrame
 
@@ -29,7 +42,7 @@ Main:
 
 ;--------------DATA---------------
 
-String: db 'I am inside the box', 0Ah, 0Ah, 0Ah, '                 please, help me', 0h
+String: db 'I am inside the box', 0Ah, 0Ah, 0Ah, '                 please, help me', END_TEXT
 
 Base: db '@-@|=|@-@'
 
@@ -50,6 +63,105 @@ SetVideoseg     proc
 
         mov bx, VIDEOSEG
         mov es, bx
+        ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; The function used ARGV
+; and set it to registers
+;
+; Entry:  ARGC_INDEX, ARGV_INDEX, SEP_CHAR
+; Exit:   AH, AL, BX, DX
+; Destrs: None
+;---------------------------------
+
+GetArgs     proc
+
+        mov al, byte ptr ds:[ARGC_INDEX]
+
+wwIf:
+        cmp al, 0h
+        je wwError
+
+        mov bx, ARGV_INDEX
+
+        call StoI
+        push ax
+
+        call StoI
+        push ax
+
+        mov cx, bx
+
+wwCond_1:
+        mov dl, byte ptr [bx]
+        cmp dl, SEP_CHAR
+        je wwStopSkipping
+
+wwCond_2:
+        cmp dl, END_SYM
+        je wwError
+
+wwWhile:
+        inc bx
+
+        jmp wwCond_1
+
+wwStopSkipping:
+
+        inc bx
+        mov dx, bx
+
+        mov bx, cx
+
+        pop ax
+        mov ah, al
+        pop cx
+        mov al, cl
+
+        ret
+wwError:
+        call ErrorHandle
+
+        ret
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; String [CX] to number AL
+;
+; Entry:  BX, SEP_CHAR
+; Exit:   AX
+; Destrs: CX, BX (skip number with space after it)
+;---------------------------------
+
+StoI     proc
+
+        mov al, 0h
+        mov ah, 0h
+        mov ch, 0h
+wwCond:
+        mov cl, byte ptr [bx]
+        cmp cl, SEP_CHAR
+        je wwEnd
+
+wwFor:
+        imul ax, ax, 0Ah                ; ax = ax * 10
+        sub cl, '0'
+        add ax, cx
+        inc bx
+
+        jmp wwCond
+
+wwEnd:
+        inc bx
+
         ret
 
 endp
@@ -159,7 +271,7 @@ wwCond:
         dec cl
         sub cl, ch
         sub cl, 01h
-        cmp dh, cl
+        cmp dh, cl                      ; compare gaps from left and right border
         pop cx
         je wwFor_end
 
@@ -205,7 +317,7 @@ endp
 
 
 ;---------------------------------
-; Print the text pointed in dx
+; Print the text pointed by DX
 ; to the frame with
 ; starting coordinates
 ; CL - x, CH - y
@@ -233,7 +345,7 @@ PrintText     proc
 
         mov ah, 0h
 wwCond:
-        cmp byte ptr [si], 0h
+        cmp byte ptr [si], END_TEXT
         je wwEnd
         cmp byte ptr [si], 0Ah          ; \n
         je wwNewLine
@@ -263,5 +375,30 @@ wwNewLine:
 endp
 
 ;--------------------------------
+
+
+;---------------------------------
+; Error message
+;
+; Entry:  None
+; Exit:   None
+; Destrs: AH, DX
+;---------------------------------
+
+ErrorHandle     proc
+
+        mov dx, offset ErrorMessage
+        mov ah, 09h
+        int 21h
+
+        mov ax, 4c00h
+        int 21h
+
+        ret
+endp
+
+;--------------------------------
+
+ErrorMessage: db 'You did not print arguments for the frame$'
 
 end Main
