ad683d1f793a6ca25ec9e28275c7d218d924af4f
dinichthys <den2005425@gmail.com>
2025-02-21 01:45:23 +0300

    Shadow

--- ramka.asm
+++ ramka.asm
@@ -10,8 +10,8 @@ LEFT_GAP        equ 3h
 
 END_TEXT        equ 0Dh
 
-HORIZON_TER     equ 50h
-VERTICAL_TER    equ 19h
+TERMINAL_LENGTH equ 50h
+TERMINAL_HEIGHT equ 19h
 
 ARGC_INDEX      equ 80h
 
@@ -20,70 +20,39 @@ ARGV_INDEX      equ 82h
 SEP_CHAR        equ ' '         ; 32h = ' '
 END_SYM         equ 0Dh         ; 0Dh = '\r'
 
+SHADOW_BIT      equ 00000111
+
 ;----------MAIN-------------------
 Main:
-        call GetArgs                    ; format: x y 'base' 'string'
+        call GetArgs                    ; format: len height 'base' color (binary) 'string'
+                                        ; ramka.com 20 10 123456789 01001111 Roflan Privet
 
         push bx
         call SetVideoseg
         pop bx
 
-        ; mov ah, 0Bh                     ; 10 * 10  | AL - x | AH - y |
-        ; mov al, 30h                     ;
-        ; mov dx, offset String           ; Text in dx
-        ; mov bx, offset Base             ; Base of the Frame in bx
-
-        call DrawFrame
+        call MakeFrame
 
         mov ax, 4c00h
         int 21h         ; exit
 
 ;---------------------------------
 
-;--------------DATA---------------
-
-String: db 'I am inside the box', 0Ah, 0Ah, 0Ah, '                 please, help me', END_TEXT
-
-Base: db '@-@|=|@-@'
-
-;---------------------------------
-
 ;-----------FUNCTIONS-------------
-
-;---------------------------------
-; The function used VIDEOSEG
-; and set it to es
-;
-; Entry:  VIDEOSEG
-; Exit:   None
-; Destrs: BX
-;---------------------------------
-
-SetVideoseg     proc
-
-        mov bx, VIDEOSEG
-        mov es, bx
-        ret
-
-endp
-
-;--------------------------------
-
 
 ;---------------------------------
 ; The function used ARGV
 ; and set it to registers
 ;
 ; Entry:  ARGC_INDEX, ARGV_INDEX, SEP_CHAR
-; Exit:   AH, AL, BX, DX
-; Destrs: None
+; Exit:   AH, AL, BX, DX, CS:FRAME_COLOR
+; Destrs: CX
 ;---------------------------------
 
 GetArgs     proc
 
         mov al, byte ptr ds:[ARGC_INDEX]
 
-wwIf:
         cmp al, 0h
         je wwError
 
@@ -95,35 +64,27 @@ wwIf:
         call StoI
         push ax
 
-        mov cx, bx
+        push bx
 
-wwCond_1:
-        mov dl, byte ptr [bx]
-        cmp dl, SEP_CHAR
-        je wwStopSkipping
+        add bx, 09h
+        call SkipSpace
 
-wwCond_2:
-        cmp dl, END_SYM
-        je wwError
+        call StoB
+        mov cs:Frame_color, al
 
-wwWhile:
-        inc bx
-
-        jmp wwCond_1
-
-wwStopSkipping:
-
-        inc bx
         mov dx, bx
 
-        mov bx, cx
+        pop bx
 
         pop ax
         mov ah, al
         pop cx
         mov al, cl
 
+        call SetCoordinates
+
         ret
+
 wwError:
         call ErrorHandle
 
@@ -134,7 +95,7 @@ endp
 
 
 ;---------------------------------
-; String [CX] to number AL
+; String [BX] to number AL
 ;
 ; Entry:  BX, SEP_CHAR
 ; Exit:   AX
@@ -143,8 +104,7 @@ endp
 
 StoI     proc
 
-        mov al, 0h
-        mov ah, 0h
+        mov ax, 0h
         mov ch, 0h
 wwCond:
         mov cl, byte ptr [bx]
@@ -160,9 +120,190 @@ wwFor:
         jmp wwCond
 
 wwEnd:
-        inc bx
+        call SkipSpace
 
         ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; String [BX] to number AL (byte in binary)
+;
+; Entry:  BX, SEP_CHAR
+; Exit:   AX
+; Destrs: CX, BX (skip number with space after it)
+;---------------------------------
+
+StoB     proc
+
+        mov ax, 0h
+        mov ch, 0h
+wwCond:
+        mov cl, byte ptr [bx]
+        cmp cl, SEP_CHAR
+        je wwEnd
+
+wwFor:
+        shl ax, 01h
+        sub cl, '0'
+        add ax, cx
+        inc bx
+
+        jmp wwCond
+
+wwEnd:
+        call SkipSpace
+
+        ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; Skip spaces in [BX]
+;
+; Entry:  BX, SEP_CHAR
+; Exit:   None
+; Destrs: CX, BX (skip spaces)
+;---------------------------------
+
+SkipSpace     proc
+
+wwCond:
+        mov cl, byte ptr [bx]
+        cmp cl, SEP_CHAR
+        jne wwEnd
+
+wwWhile:
+        inc bx
+
+        jmp wwCond
+
+wwEnd:
+
+        ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; Set coordinates to CX (CL - x, CH - y)
+; from size in AX (AL - length, AH - height)
+;
+; Entry:  AX
+; Exit:   CX
+; Destrs: None
+;---------------------------------
+
+SetCoordinates     proc
+
+    push ax
+
+    shr al, 01h
+    shr ah, 01h
+
+    mov cl, TERMINAL_LENGTH
+    shr cl, 01h
+    mov ch, TERMINAL_HEIGHT
+    shr ch, 01h
+
+    sub cl, al
+    sub ch, ah
+
+    pop ax
+
+    ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; The function used VIDEOSEG
+; and set it to es
+;
+; Entry:  VIDEOSEG
+; Exit:   None
+; Destrs: BX
+;---------------------------------
+
+SetVideoseg     proc
+
+    mov bx, VIDEOSEG
+    mov es, bx
+    ret
+
+endp
+
+;--------------------------------
+
+
+;---------------------------------
+; Draw the frame with text
+;
+; Entry:  ES, AH, AL, BX, CX, DX, CS:FRAME_COLOR
+; Exit:   None
+; Destrs: AX, CX, DI
+;---------------------------------
+
+MakeFrame     proc
+
+    mov di, ax
+
+    cmp ah, al
+    jb wwLess
+    ja wwMore
+
+    mov ah, 01h
+    mov al, 01h
+
+wwCond:
+    cmp di, ax
+    jb wwDone
+
+wwWhile:
+    call SetCoordinates
+    push dx cx bx ax
+    call DrawFrame
+    pop ax bx cx
+    push cx bx ax
+    call DrawShadow
+    mov cx, 2h
+    mov dx, 0h
+    mov ah, 86h
+    int 15h
+    pop ax bx cx dx
+    inc ah
+    inc al
+    jmp wwCond
+
+wwDone:
+    dec ah
+    dec al
+
+    call PrintText
+
+    ret
+
+wwLess:
+    sub al, ah
+    add al, 01h
+    mov ah, 01h
+    jmp wwCond
+
+wwMore:
+    sub ah, al
+    add ah, 01h
+    mov al, 01h
+    jmp wwCond
 
 endp
 
@@ -171,66 +312,115 @@ endp
 
 ;---------------------------------
 ; The function draw the frame
-; with the text pointed by DX
-; and base pointed by BX
-; and size in AX (AL - x, AH - y)
+; with the text pointed by DX,
+; base pointed by BX,
+; size in AX (AL - x, AH - y),
+; coordinates in CX
+; (CL - x, CH - y)
+; and color in DL
 ;
-; Entry:  ES, AH, AL, BX, DX
+; Entry:  ES, AH, AL, BX, CX, DX, CS:FRAME_COLOR
 ; Exit:   None
-; Destrs: CX
+; Destrs: AX, CX, BX
 ;---------------------------------
 
 DrawFrame     proc
 
-        push dx
+    add ah, ch
 
-        mov cl, HORIZON_TER             ; 50h = 80
-        sub cl, al
-        shr cl, 01h
-        add al, cl
-        dec al
+    dec ah
+    mov cs:Bottom_Coordinates, ah
+    mov ah, cs:Frame_color
 
-        mov ch, VERTICAL_TER             ; 19h = 25
-        sub ch, ah
-        shr ch, 01h
-        add ah, ch
-        dec ah
-
-        push cx                 ; Save the starting coordinates
-
-        push ax
-        call BaseLine
-        pop ax
-        inc ch
+    call BaseLine
+    inc ch
 
 wwCond:
-        cmp ah, ch
-        je wwEnd
+    cmp cs:Bottom_Coordinates, ch
+    jbe wwEnd
 
 wwFor:
-        push ax
-        call BaseLine
-        pop ax
-        inc ch
-        sub bx, 03h
+    call BaseLine
+    inc ch
+    sub bx, 03h
 
-        jmp wwCond
+    jmp wwCond
 
 wwEnd:
+    dec ch
+    cmp cs:Bottom_Coordinates, ch
+    jbe wwStop
 
-        add bx, 03h
-        push ax
-        call BaseLine
-        pop ax
+    inc ch
+    add bx, 03h
+    call BaseLine
 
-        pop cx
-        pop dx
+wwStop:
+    ret
 
-        push ax
-        call PrintText
-        pop ax
+endp
 
-        ret
+;--------------------------------
+
+Bottom_Coordinates db 0h
+
+Frame_color db 0h
+
+;---------------------------------
+; The function draw the shadow of the frame
+; with size in AX (AL - x, AH - y)
+; and coordinates in CX (CL - x, CH - y)
+;
+; Entry:  ES, AH, AL, CX, CS:FRAME_COLOR
+; Exit:   None
+; Destrs: AX, BX
+;---------------------------------
+
+DrawShadow     proc
+
+    push ax cx
+
+    mov bl, ch
+    mov bh, 0h
+    inc bx
+
+    imul bx, bx, TERMINAL_LENGTH
+    mov ch, 0h
+    add bx, cx
+    mov ah, 0h
+    add bx, ax
+
+    pop cx ax
+
+    shl bx, 01h
+    inc bx                              ; bx - point on the right top element of the shadow on the screen
+
+wwCond_Vertical:
+    cmp ah, 01h
+    jbe wwEnd_Vertical
+
+wwFor_Vertical:
+    mov byte ptr es:[bx], SHADOW_BIT
+    add bx, TERMINAL_LENGTH * 2
+    dec ah
+    jmp wwCond_Vertical
+
+wwEnd_Vertical:
+
+
+wwCond_Horizontal:
+    cmp al, 0h
+    je wwEnd_Horizontal
+
+wwFor_Horizontal:
+    mov byte ptr es:[bx], SHADOW_BIT
+    sub bx, 02h
+    dec al
+    jmp wwCond_Horizontal
+
+wwEnd_Horizontal:
+
+    ret
 
 endp
 
@@ -239,82 +429,73 @@ endp
 
 ;---------------------------------
 ; The function draw the line of the frame
-; with coordinates in CX (CL - x, CH - y)
+; with coordinates in CX (CL - x, CH - y),
+; color in AH, length in AL and base in [bx]
 ;
-; Entry:  ES, CH, CL, BX, WIDTH_STR
+; Entry:  ES, CH, CL, BX, AH, AL
 ; Exit:   None
-; Destrs: AX, DX, BX (inc x3)
+; Destrs: DX, BX (inc x3)
 ;---------------------------------
 
 BaseLine     proc
 
-        mov dl, [bx]
-        push bx
-        mov bl, ch
-        mov bh, 0h
-        imul bx, bx, HORIZON_TER
-        mov al, cl
-        mov ah, 0h
-        add bx, ax
-        shl bx, 01h                     ; bx = (ch * 80 + cl) * 2
-        mov byte ptr es:[bx], dl
-        pop bx
+    cld
 
-        mov dh, cl
-        inc bx
+    push dx cx ax
 
-wwCond:
-        push cx
-        mov ch, cl
-        mov cl, HORIZON_TER
-        dec cl
-        dec cl
-        sub cl, ch
-        sub cl, 01h
-        cmp dh, cl                      ; compare gaps from left and right border
-        pop cx
-        je wwFor_end
+    mov dl, ch
+    mov dh, 0h
+    imul dx, dx, TERMINAL_LENGTH
 
-wwFor:
-        inc cl
-        mov dl, [bx]
-        push bx
-        mov bl, ch
-        mov bh, 0h
-        imul bx, bx, HORIZON_TER
-        mov al, cl
-        mov ah, 0h
-        add bx, ax
-        shl bx, 01h                     ; bx = (ch * 80 + cl) * 2
-        mov byte ptr es:[bx], dl
-        pop bx
+    mov al, cl
+    mov ah, 0h
+    add dx, ax
 
-        jmp wwCond
+    pop ax
+    push ax
+    mov cl, al
+    mov ch, 0h
 
-wwFor_end:
-        inc bx
+    shl dx, 01h                     ; dx = (ch * 80 + cl) * 2
 
-        mov dl, [bx]
-        push bx
-        mov bl, ch
-        mov bh, 0h
-        imul bx, bx, HORIZON_TER
-        mov al, cl
-        mov ah, 0h
-        add bx, ax
-        shl bx, 01h                     ; bx = (ch * 80 + cl) * 2
-        mov byte ptr es:[bx], dl
-        pop bx
+    mov di, dx
 
-        mov cl, dh
-        inc bx
+    mov dl, al
+    cmp dl, 01h
+    jb wwEnd_Half_Len
 
-        ret
+    mov al, cs:[bx]
+    stosw
+
+    cmp dl, 02h
+    jb wwEnd_Half_Len
+
+    sub cx, 02h
+    inc bx
+    mov al, cs:[bx]
+    rep stosw
+
+    inc bx
+    mov al, cs:[bx]
+    stosw
+
+    inc bx
+
+    pop ax cx dx
+
+    ret
+
+wwEnd_Half_Len:
+    add bx, 03h
+    pop ax cx dx
+
+    ret
 
 endp
 
 ;--------------------------------
 
+; -------TEXT---------
 
 ;---------------------------------
 ; Print the text pointed by DX
@@ -324,53 +505,63 @@ endp
 ;
 ; Entry:  ES, DX, CH, CL
 ; Exit:   None
-; Destrs: AX, BX
+; Destrs: AX, BX, SI, DI
 ;---------------------------------
 
 PrintText     proc
 
-        mov bl, ch
-        mov bh, 0h
-        add bx, TOP_GAP
-        imul bx, bx, HORIZON_TER
-        shl bx, 01h
-        mov al, cl
-        mov ah, 0h
-        shl ax, 01h
-        add bx, ax                      ; bx - index in memory of starting the string
-        add bx, LEFT_GAP * 2
+    cld
 
-        push si
-        mov si, dx
+    ; mov bl, ch
+    ; mov bh, 0h
+    ; add bx, TOP_GAP + 1
+    ; imul bx, bx, TERMINAL_LENGTH
+    ; mov al, cl
+    ; mov ah, 0h
+    ; add bx, ax
+    ; shl bx, 01h
+    ; add bx, LEFT_GAP * 2 + 2            ; bx - index in memory of starting the string
+    mov bx, TERMINAL_HEIGHT
+    shr bx, 01h
+    imul bx, bx, TERMINAL_LENGTH
+    mov al, cl
+    mov ah, 0h
+    add bx, ax
+    add bx, LEFT_GAP + 1
+    shl bx, 01h
 
-        mov ah, 0h
+    mov di, bx
+    mov si, dx
+
+    push ds
+    mov ax, cs
+    mov ds, ax
+    mov ax, 0h
 wwCond:
-        cmp byte ptr [si], END_TEXT
-        je wwEnd
-        cmp byte ptr [si], 0Ah          ; \n
-        je wwNewLine
+    cmp byte ptr cs:[si], END_SYM
+    je wwEnd
+    cmp byte ptr cs:[si], 0Ah          ; \n
+    je wwNewLine
 
 wwWhile:
-        inc ah
-        mov al, byte ptr [si]
-        mov byte ptr es:[bx], al
-        add bx, 02h
-        inc si
+    inc ax
+    movsb
+    inc di
 
-        jmp wwCond
+    jmp wwCond
 
 wwEnd:
+    pop ds
 
-        pop si
-        ret
+    ret
 
 wwNewLine:
-        inc si
-        add bx, horizon_ter * 2
-        shl ah, 01h
-        sub bl, ah
-        mov ah, 0h
-        jmp wwCond
+    inc si
+    add di, TERMINAL_LENGTH * 2
+    shl ax, 01h
+    sub di, ax
+    mov ax, 0h
+    jmp wwCond
 
 endp
 
@@ -399,6 +590,6 @@ endp
 
 ;--------------------------------
 
-ErrorMessage: db 'You did not print arguments for the frame$'
+ErrorMessage: db 'You did not enter arguments for the frame$'
 
 end Main
